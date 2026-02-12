f4672dc5b50765b61b4ff3af6b0e460b68f1ed63
softworldqjin <softworldqjin@gmail.com>
2026-02-12 16:27:33 +0900

    LinkedList

--- src/collection/link/MyLinkedListV2Main.java
+++ src/collection/link/MyLinkedListV2Main.java
@@ -2,6 +2,17 @@ package collection.link;
 
 public class MyLinkedListV2Main {
     public static void main(String[] args) {
+        MyLinkedListV2 list = new MyLinkedListV2();
+        list.add("a");
+        list.add("b");
+        list.add("c");
 
+        System.out.println(list);
+
+        list.add(1, "hello");
+        System.out.println(list);
+
+        list.remove(1);
+        System.out.println(list);
     }
 }
